d479199dbbebf9e2fbb4fda172c9e32cdb461276
huydat-isme <datprono28@gmail.com>
2024-01-03 00:38:48 +0700

    update header

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html","./stylec.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
+
